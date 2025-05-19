2d03692ef0f9f1af2b05b55b8ca1f46e72585670
RISHABH SHAH <rnshah1997@gmail.com>
2025-05-19 15:53:17 -0400

    CartUpdate

--- src/Store/cartSlice.js
+++ src/Store/cartSlice.js
@@ -7,22 +7,27 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title,
+          name: newItem.title || newItem.name,
         });
       } else {
         existingItem.quantity = existingItem.quantity + 1;
-        existingItem.totalPrice = newItem.totalPrice + newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.finc((item) => item.id === id);
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (!existingItem) {
+        return;
+      }
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
---
+++ src/components/Shop/Data.js
@@ -0,0 +1,28 @@
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First book ",
+    description: " The first book i wrote",
+  },
+  {
+    id: "p2",
+    price: 7,
+    title: "My Second book ",
+    description: " The first book i wrote",
+  },
+  {
+    id: "p3",
+    price: 8,
+    title: "My Third book ",
+    description: " The first book i wrote",
+  },
+  {
+    id: "p4",
+    price: 10,
+    title: "My Fourth book ",
+    description: " The Fourth book i wrote",
+  },
+];
+
+export default DUMMY_PRODUCTS;
